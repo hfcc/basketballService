a04a66f54e44624f3f6d6449b98afbe34a59d910
hanfeng <feng.han@merico.dev>
2022-10-26 00:30:06 +0800

    [cuican][feature] get data from database

--- db/dao/game.go
+++ db/dao/game.go
@@ -3,14 +3,12 @@ package dao
 import (
 	"basketballService/db"
 	"basketballService/db/model"
-	"fmt"
 )
 
 // Getschedules
-func Getschedules() (*[]model.ScheduleModel, error) {
-	var schedules *[]model.ScheduleModel
+func Getschedules() ([]model.ScheduleModel, error) {
+	var schedules []model.ScheduleModel
 	cli := db.Get()
-	fmt.Print(cli)
 	var result = cli.Table("schedule").Find(&schedules)
 	return schedules, result.Error
 }	
--- service/gameService.go
+++ service/gameService.go
@@ -3,11 +3,16 @@ package service
 import (
 	"basketballService/db/dao"
 	"basketballService/db/model"
+
+	"github.com/samber/lo"
 )
-func Getschedules() (*[]model.ScheduleModel, error) {
-	schedule, err := dao.Getschedules()
+func Getschedules() (map[string][]model.ScheduleModel, error) {
+	schedules, err := dao.Getschedules()
 	if err != nil {
 		return nil, err
 	}
-	return schedule, nil
+	schedulesInDay := lo.GroupBy(schedules, func (schedule model.ScheduleModel) string  {
+		return schedule.StartTimeUTC.Format("2006-01-02")
+	})
+	return schedulesInDay, nil
 }
